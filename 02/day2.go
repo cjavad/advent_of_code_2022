93326062153b4a9e5a8032359ac1bc786b8dc3d7
Javad Asgari Shafique <javadshafique@hotmail.com>
2022-12-02 08:22:26 +0100

    Day 2! With some modules :p

---
+++ 02/day2.go
@@ -0,0 +1,125 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+
+	"golang.org/x/exp/slices"
+)
+
+func main() {
+	p1 := part1()
+	p2 := part2()
+	fmt.Println("Part 1: ", p1)
+	fmt.Println("Part 2: ", p2)
+}
+
+func part1() int {
+	lines := read_input("input.txt")
+	sum := 0
+	for _, line := range lines {
+		sum += score_from_string(line)
+	}
+	return sum
+}
+
+func part2() int {
+	lines := read_input("input.txt")
+	sum := 0
+	for _, line := range lines {
+		sum += reverse_score_from_string(line)
+	}
+	return sum
+}
+
+func read_input(inputfile string) []string {
+	input, err := os.Open(inputfile)
+	if err != nil {
+		panic(err)
+	}
+	defer input.Close()
+	var lines []string
+	scanner := bufio.NewScanner(input)
+	for scanner.Scan() {
+		lines = append(lines, scanner.Text())
+	}
+	return lines
+}
+
+func print_output(x int) {
+	switch x {
+	case 1:
+		fmt.Print("Rock")
+	case 2:
+		fmt.Print("Paper")
+	case 3:
+		fmt.Print("Scissors")
+	}
+}
+
+func reverse_score_from_string(s string) int {
+	lookup := [][]int{
+		{0, -1, 1},
+		{1, 0, -1},
+		{-1, 1, 0},
+	}
+	r := []rune(s)
+	e, c := int(rune(r[0]))-64, int(rune(r[2]))-87
+	idx := int(0)
+	p := 3 * (c - 1)
+	print_output(e)
+
+	switch c {
+	case 1:
+
+		fmt.Print(" needs to lose against ")
+		idx = slices.Index(lookup[e-1], 1)
+	case 2:
+		p = 3
+		fmt.Print(" needs to tie against ")
+		idx = slices.Index(lookup[e-1], 0)
+	case 3:
+		fmt.Print(" needs to win against ")
+		idx = slices.Index(lookup[e-1], -1)
+	default:
+		panic("Invalid input")
+	}
+
+	p += idx + 1
+
+	print_output(idx + 1)
+	fmt.Printf(" for %d points\n", p)
+
+	return p
+}
+
+func score_from_string(s string) int {
+	lookup := [][]int{
+		{0, -1, 1},
+		{1, 0, -1},
+		{-1, 1, 0},
+	}
+
+	r := []rune(s)
+	e, y := int(rune(r[0]))-64, int(rune(r[2]))-87
+	p := y
+
+	print_output(e)
+
+	switch lookup[y-1][e-1] {
+	case 1:
+		p += 6
+		fmt.Print(" beats ")
+	case -1:
+		fmt.Print(" loses to ")
+	case 0:
+		p += 3
+		fmt.Print(" ties ")
+	}
+
+	print_output(y)
+	fmt.Printf(" for %d points\n", p)
+
+	return p
+}
---
+++ 02/go.mod
@@ -0,0 +1,5 @@
+module github.com/cjavad/advent_of_code_2022/02
+
+go 1.19
+
+require golang.org/x/exp v0.0.0-20221126150942-6ab00d035af9
